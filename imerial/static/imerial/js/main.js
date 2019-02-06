b22356eef722a4ec3cde38c983f8c317094bb77f
Zetinator <erick_zetina@hotmail.com>
2019-02-06 16:52:28 -0600

    search bar working

--- imerial/static/imerial/js/main.js
+++ imerial/static/imerial/js/main.js
@@ -138,58 +138,58 @@
     };
 
 
-    /* search
-    * ------------------------------------------------------ */
-    var clSearch = function() {
-
-        var searchWrap = $('.page-header__search'),
-            searchField = searchWrap.find('.search-field'),
-            closeSearch = searchWrap.find('.page-header__overlay-close'),
-            searchTrigger = $('.page-header__search-trigger'),
-            siteBody = $('body');
-
-
-        searchTrigger.on('click', function(e) {
-
-            e.preventDefault();
-            e.stopPropagation();
-
-            var $this = $(this);
-
-            siteBody.addClass('search-is-visible');
-            setTimeout(function(){
-                searchWrap.find('.search-field').focus();
-            }, 100);
-
-        });
-
-        closeSearch.on('click', function(e) {
-
-            var $this = $(this);
-
-            e.stopPropagation();
-
-            if(siteBody.hasClass('search-is-visible')){
-                siteBody.removeClass('search-is-visible');
-                setTimeout(function(){
-                    searchWrap.find('.search-field').blur();
-                }, 100);
-            }
-        });
-
-        searchWrap.on('click',  function(e) {
-            if( !$(e.target).is('.search-field') ) {
-                closeSearch.trigger('click');
-            }
-        });
-
-        searchField.on('click', function(e){
-            e.stopPropagation();
-        });
-
-        searchField.attr({placeholder: '', autocomplete: 'off'});
-
-    };
+    // /* search
+    // * ------------------------------------------------------ */
+    // var clSearch = function() {
+    //
+    //     var searchWrap = $('.page-header__search'),
+    //         searchField = searchWrap.find('.search-field'),
+    //         closeSearch = searchWrap.find('.page-header__overlay-close'),
+    //         searchTrigger = $('.page-header__search-trigger'),
+    //         siteBody = $('body');
+    //
+    //
+    //     searchTrigger.on('click', function(e) {
+    //
+    //         e.preventDefault();
+    //         e.stopPropagation();
+    //
+    //         var $this = $(this);
+    //
+    //         siteBody.addClass('search-is-visible');
+    //         setTimeout(function(){
+    //             searchWrap.find('.search-field').focus();
+    //         }, 100);
+    //
+    //     });
+    //
+    //     closeSearch.on('click', function(e) {
+    //
+    //         var $this = $(this);
+    //
+    //         e.stopPropagation();
+    //
+    //         if(siteBody.hasClass('search-is-visible')){
+    //             siteBody.removeClass('search-is-visible');
+    //             setTimeout(function(){
+    //                 searchWrap.find('.search-field').blur();
+    //             }, 100);
+    //         }
+    //     });
+    //
+    //     searchWrap.on('click',  function(e) {
+    //         if( !$(e.target).is('.search-field') ) {
+    //             closeSearch.trigger('click');
+    //         }
+    //     });
+    //
+    //     searchField.on('click', function(e){
+    //         e.stopPropagation();
+    //     });
+    //
+    //     searchField.attr({placeholder: '', autocomplete: 'off'});
+    //
+    // };
 
 
    /* slick slider
@@ -358,7 +358,7 @@
         clMenuOnScrolldown();
         clMobileMenu();
         clWaypoints();
-        clSearch();
+        // clSearch();
         clSlickSlider();
         clSmoothScroll();
         clAlertBoxes();
